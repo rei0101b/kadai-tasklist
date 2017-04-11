21601a6dabec71743d7cb05f8f9eacf6db2b25ed
rei.yamaguchi <rei0101b@gmail.com>
2017-04-11 18:55:03 +0000

    tasklist complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
-
     def index
-        @tasks = Task.all
+        @tasks = Task.all.page(params[:page]).per(12)
     end
     
     def show 
-        set_task
+        @task = Task.find(params[:id])
     end
     
     def new
@@ -25,11 +23,11 @@ class TasksController < ApplicationController
     end
     
     def edit 
-        set_task
+        @task = Task.find(params[:id])
     end
     
     def update
-        set_task
+        @task = Task.find(params[:id])
         if @task.update(task_params)
             flash[:success] = "Taskは正常に更新されました"
             redirect_to @task
@@ -40,7 +38,7 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        set_task
+        @task = Task.find(params[:id])
         @task.destroy
         flash[:success] = "Taskは正常に削除されました"
         redirect_to tasks_url
@@ -48,11 +46,6 @@ class TasksController < ApplicationController
 end
 
 private
-
-def set_task
-    @task = Task.find(params[:id])
-end
-
 # Strong Parameter
 def task_params
     params.require(:task).permit(:content, :person, :status)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..100).each do |number|
+    person = ["予定", "実行中","完了","保留","問題"]
+    status = ["父","母","兄","妹"]
+    Task.create(content: "仕事:" + number.to_s, status: status[number%5], person: person[number%4] )
+end
+
+# (1..100).each do |number|
+#     Task.create(content: "仕事:" + number.to_s, status: "予定", person: "兄" )
+# end
